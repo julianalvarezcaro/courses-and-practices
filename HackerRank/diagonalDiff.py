10718a76bce7a16066510f5cba78302a92d1affa
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-04-18 10:37:07 -0500

    Completed a new challenge!

---
+++ HackerRank/diagonalDiff.py
@@ -0,0 +1,23 @@
+# Link to the challenge:
+# https://www.hackerrank.com/challenges/diagonal-difference/problem
+
+#Summary:
+# Find the absolute difference between the sums of the diagonals of a square matrix
+
+def diagonalDifference(arr):
+    lrDiagSum = sum([ arr[x][x] for x in range(len(arr)) ])
+    rlDiagSum = sum([ row[-n-1] for n,row in enumerate(arr) ])
+
+    return (abs(lrDiagSum - rlDiagSum))
+
+if __name__ == '__main__':
+    #! Just a test case
+    array = [
+        [1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0],
+        [0, 0, 2, 4, 4, 0],
+        [0, 0, 0, 2, 0, 0],
+        [0, 0, 1, 2, 4, 0]
+        ]
+    print(diagonalDifference(array))
