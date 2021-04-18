894eab51ec941af22928354d27ce3a80fc45511b
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-04-18 00:41:57 -0500

    Uploading HackerRank folder

---
+++ HackerRank/hourGlass.py
@@ -0,0 +1,16 @@
+# Link to the challenge: 
+# https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&isFullScreen=false&playlist_slugs%5B%5D%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D%5B%5D=arrays
+
+# Summary:
+# In a given a 6x6 array are 16 sets of 'hourglasses', like this:
+# a b c
+# - d -
+# e f g
+
+#The problem consists of finding all the sums of the different hourglasses and returning the biggest result of them all.
+
+
+def hourglassSum(arr):
+    # Takes in a 6x6 array
+    # Return: greater possible sum among all the possible hourglasses
+    pass
---
+++ HackerRank/requestTime.py
@@ -0,0 +1,52 @@
+# This problem was part of a technical challenge. I could NOT complete this task
+# I didn't save the problem statement nor test cases
+
+
+# The proble was something like this:
+
+
+
+def droppedRequests(requestTime):
+    dropped = 0
+    ocurr = 0
+    past = requestTime[0]
+    pos = 0
+
+    tenSec = 0
+    minSec = 0
+    if len(requestTime) == 1:
+        return dropped
+    for current in requestTime:
+        # print(f'In a minute: {len(requestTime[minSec:pos])}')
+        # print(minSec)
+        if len(requestTime[minSec:pos]) > 60:
+            # print("Minute")
+            dropped += 1
+        elif len(requestTime[tenSec:pos]) > 20:
+            # print("10 seconds")
+            dropped += 1
+        else:
+            if current == past:
+                ocurr += 1
+            else:
+                past = current
+                ocurr = 1
+            if ocurr > 3:
+                dropped += 1
+        while(requestTime[pos] - requestTime[tenSec] > 10):
+            # print("Im in ten")
+            # print (f'pos - tenSec = {pos} - {tenSec} = {pos - tenSec}')
+            if tenSec == pos - 1:
+                break
+            tenSec += 1
+        while(requestTime[pos] - requestTime[minSec] > 59):
+            #print("Im in min")
+            if minSec == pos - 1:
+                break
+            minSec += 1
+        pos += 1
+    return dropped
+
+if __name__ == '__main__':
+    arr = [1,1,1,1,2]
+    print(droppedRequests(arr))
