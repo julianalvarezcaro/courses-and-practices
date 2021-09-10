791945a669f13177df36024a9d5b2d33ae9080fe
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-09-09 22:26:28 -0500

    Now using Flask's session

--- Platzi/Flask/main.py
+++ Platzi/Flask/main.py
@@ -1,12 +1,20 @@
-from flask import Flask, make_response, request, redirect, render_template
+from flask import Flask, make_response, request, redirect, render_template, session
 from flask_bootstrap import Bootstrap, bootstrap_find_resource
-
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-
 bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = 'SUPER_SPY'
+
 todos = ['Comprar pan', 'Comprar leche', 'Comprar lo demás']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -25,16 +33,19 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    # response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
-        'todos': todos
+        'todos': todos,
+        'login_form': login_form
     }
 
     return render_template('hello.html', **context)
