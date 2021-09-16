0c82a29c0ef7b367fac5386d08c7ff86eef23949
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-09-16 13:12:41 -0500

    Implemented signup

--- Platzi/Flask/app/firestore_service.py
+++ Platzi/Flask/app/firestore_service.py
@@ -11,12 +11,19 @@ def get_users():
     """
     return db.collection('users').get()
 
+
 def get_user(user_id):
     """Returns a user by id
     """
     return db.collection('users').document(user_id).get()
 
+
 def get_todos(user_id):
     """Returns todos of an user
     """
     return db.collection('users').document(user_id).collection('todos').get()
+
+
+def user_put(user_data):
+    user_ref = db.collection('users').document(user_data.username)
+    user_ref.set({'password': user_data.password})
